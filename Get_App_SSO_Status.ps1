02c1faf94ce0eaf7f64d02160cb397f09251ce03
Mark Connelly <mark.d.connelly@outlook.com>
2023-03-29 21:30:57 -0500

    Update Get_App_SSO_Status.ps1

--- Get_App_SSO_Status.ps1
+++ Get_App_SSO_Status.ps1
@@ -1,2 +1,11 @@
 $arrAAD_Applications = @()
 $arrAAD_Applications = Get-MgServicePrincipal -All:$true | Where-Object {$_.Tags -eq "WindowsAzureActiveDirectoryIntegratedApp"}
+$app = $null
+$strAppDisplayName = ""
+$strEntAppObjectID = ""
+$strAppID = ""
+foreach ($app in $arrAAD_Applications) {
+    $strAppDisplayName = $app.DisplayName 
+    $strEntAppObjectID = $app.Id
+    $strAppID = $app.AppId
+}
